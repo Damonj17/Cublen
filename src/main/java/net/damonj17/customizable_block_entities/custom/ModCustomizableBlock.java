a130bf7fa20f870a25c1af0f6271dd56014058a6
Damon <damonj17@hotmail.com>
2024-04-21 21:04:40 -0400

    no block entities for now.

--- src/main/java/net/damonj17/customizable_block_entities/custom/ModCustomizableBlock.java
+++ src/main/java/net/damonj17/customizable_block_entities/custom/ModCustomizableBlock.java
@@ -3,6 +3,7 @@ package net.damonj17.customizable_block_entities.custom;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.FaceAttachedHorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
@@ -10,6 +11,12 @@ import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.AttachFace;
 import net.minecraft.world.Nameable;
 
+/* I hope minecraft forge explodes.
+Todo:
+net.minecraft.world.level.block.Blocks
+I'd like this block to keep its name in an Anvil, similarly to how Chests or Furnaces do. Currently, if I rename the block in an anvil, then place it down and mine it, the block doesn't keep its name.
+I'd like to set the block to have an Owner, such as whoever crafted the block. This is currently (as far as I'm aware) only done on the player head item in vanilla minecraft.
+ */
 public class ModCustomizableBlock extends FaceAttachedHorizontalDirectionalBlock implements Nameable{
     public ModCustomizableBlock(Properties pProperties) {
         super(pProperties);
@@ -18,6 +25,12 @@ public class ModCustomizableBlock extends FaceAttachedHorizontalDirectionalBlock
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
         pBuilder.add(FACING, FACE);
     }
+
+    @Override
+    public boolean canSurvive(BlockState pState, LevelReader pLevel, BlockPos pPos) {
+        return true;
+    }
+
     @Override
     public boolean isFlammable(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
         return false;
